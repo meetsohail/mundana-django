3d6fb25282ef4da5241e2a74505bdb665ff0eae8
Sohail Ahmed <sohail.comsians@gmail.com>
2021-10-29 23:36:03 +0500

    updated settings

--- mundanadjango/settings.py
+++ mundanadjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-csst1mpe14a=*@o2n#5qxir&_@y=y0uy8bcf3h07$%t$fm8vr6'
+SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJ_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frontend',
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -73,14 +76,24 @@ WSGI_APPLICATION = 'mundanadjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG is True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+    	}
     }
-}
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+            'NAME': os.environ.get('DJ_DB_NAME'),
+            'USER': os.environ.get('DJ_DB_USER'),
+            'PASSWORD': os.environ.get('DJ_DB_PASSWORD'),
+            'HOST': os.environ.get('DJ_DB_HOST', 'localhost'),
+            'PORT': os.environ.get('DJ_DB_PORT', ''),   
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
